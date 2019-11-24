6b63b3887dc6aa5f68905f7e47406a5b0aeaec29
Jan Cavel <caveljan@gmail.com>
2019-11-24 23:07:19 +0200

    denium > dev: Page, Pluriverse, pathChange

--- implementations/denium/source/renderer/modules/components/Page/index.tsx
+++ implementations/denium/source/renderer/modules/components/Page/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {
+    useContext,
+} from 'react';
 
 import {
     StyledPage,
 } from './styled';
 
+import PluriverseContext from '../../containers/Pluriverse/context';
+
 
 
 interface PageProperties {
-    src: string;
+    id: string;
 }
 
 const Page: React.FC<PageProperties> = (properties) => {
+    const context: any = useContext(PluriverseContext);
+
     const {
-        src,
+        pages,
+    } = context;
+
+    const {
+        id,
     } = properties;
+
+    const page = pages.find((page: any) => page.id === id);
+    if (!page) {
+        return (
+            <div>
+                <h4>
+                    Nothing Found.
+                </h4>
+            </div>
+        );
+    }
+
+    const src = page.path;
 
     return (
         <StyledPage>
