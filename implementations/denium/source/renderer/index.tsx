66608d865f1291a38863abc9d2120d2ad5f90d19
Jan Cavel <caveljan@gmail.com>
2019-11-24 12:28:34 +0200

    denium > dev: renderer

--- implementations/denium/source/renderer/index.tsx
+++ implementations/denium/source/renderer/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 
 
 ReactDOM.render(
-    <div>
-        <h4>rendered</h4>
-    </div>,
+    <App />,
     document.getElementById('denium'),
 );
