5c31f565636629d165ffcfe03780dee8ff586a91
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-11-24 16:16:50 +0200

    denium > dev: TopBar

--- implementations/denium/source/renderer/modules/components/TopBar/styled.ts
+++ implementations/denium/source/renderer/modules/components/TopBar/styled.ts
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 
 
 export const StyledTopBar: any = styled.div`
+    -webkit-app-region: drag;
+
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
-    height: 45px;
+    height: 40px;
     z-index: 9999;
     background-color: ${(props: any) => {
         if (props.mouseOver) {
