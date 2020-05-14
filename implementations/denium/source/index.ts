dd858f8ea5c5e61da6ba979ac992bd227e83f9c2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-05-14 20:45:58 +0300

    denium > dev: app

--- implementations/denium/source/index.ts
+++ implementations/denium/source/index.ts
@@ -45,10 +45,10 @@ function createWindow() {
 
     // Emitted when the window is closed.
     win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
     });
 }
 
@@ -62,7 +62,7 @@ app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-    app.quit();
+        app.quit();
     }
 });
 
@@ -70,9 +70,9 @@ app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-    createWindow();
+        createWindow();
     }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+app.allowRendererProcessReuse = true;
