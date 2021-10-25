e0c9a10e3bf468aea3a2e0e4f81ee7d4dac7f5a7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-10-25 12:45:19 +0300

    implementations/denium > dev: index

--- implementations/denium/source/index.ts
+++ implementations/denium/source/index.ts
@@ -2,9 +2,8 @@
     // #region libraries
     import {
         app,
-        // BrowserView,
+        screen,
         BrowserWindow,
-        // systemPreferences,
     } from 'electron';
     // #endregion libraries
 // #endregion imports
@@ -12,15 +11,15 @@
 
 
 // #region module
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win: any;
+let window: BrowserWindow | null = null;
 
-function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({
-        width: 1000,
-        height: 800,
+
+const createWindow = () => {
+    const mainScreen = screen.getPrimaryDisplay();
+
+    window = new BrowserWindow({
+        width: mainScreen.size.width,
+        height: mainScreen.size.height,
         icon: './assets/meta/den.png',
         minHeight: 500,
         minWidth: 300,
@@ -31,56 +30,36 @@ function createWindow() {
             webviewTag: true,
             nativeWindowOpen: true,
         },
+        show: false,
     });
-    // systemPreferences.isDarkMode();
 
-    // and load the index.html of the app.
-    win.loadFile('index.html');
+    window.loadFile('index.html');
 
-    // Open the DevTools.
-    // win.webContents.openDevTools();
+    window.once('ready-to-show', () => {
+        if (!window) {
+            return;
+        }
 
-    // const view = new BrowserView({
-    //     webPreferences: {
-    //         nodeIntegration: false,
-    //     },
-    // });
-    // win.setBrowserView(view);
-    // view.setBounds({ x: 0, y: 38, width: 1440, height: 860 });
-    // view.webContents.loadURL('https://electronjs.org');
+        window.show();
+    })
 
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null;
+    window.on('closed', () => {
+        window = null;
     });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
+    if (window === null) {
         createWindow();
     }
 });
-
-
-// app.allowRendererProcessReuse = true;
 // #endregion module
